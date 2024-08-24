d80eac95efeb6c14af5a8a79f9305ca56145b862
PeeyushVerma21 <peeyushverma00@gmail.com>
2024-08-24 14:45:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [colorMode, setColorMode] = useState("red");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      let elements = document.getElementsByClassName("cbtn");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "white";
+        elements[i].style.color = "black";
+      }
+      showAlert("Dark mode have been enabled!", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      let elements = document.getElementsByClassName("cbtn");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "black";
+        elements[i].style.color = "white";
+      }
+      showAlert("Light mode have been enabled!", "success");
+    }
+  };
+
+  const toggleColor = () => {
+    if (
+      colorMode !== "Red" &&
+      document.getElementById("flexRadioDefault1").checked
+    ) {
+      setColorMode("red");
+      document.body.style.backgroundColor = "red";
+      document.body.style.color = "black";
+      let elements = document.getElementsByClassName("cbtn");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "black";
+        elements[i].style.color = "white";
+      }
+    } else if (
+      colorMode !== "green" &&
+      document.getElementById("flexRadioDefault2").checked
+    ) {
+      setColorMode("green");
+      document.body.style.backgroundColor = "green";
+      document.body.style.color = "black";
+      let elements = document.getElementsByClassName("cbtn");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "red";
+        elements[i].style.color = "white";
+      }
+    } else if (
+      colorMode !== "blue" &&
+      document.getElementById("flexRadioDefault3").checked
+    ) {
+      setColorMode("blue");
+      document.body.style.backgroundColor = "blue";
+      document.body.style.color = "black";
+      let elements = document.getElementsByClassName("cbtn");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "lightblue";
+        elements[i].style.color = "white";
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <BrowserRouter> */}
+        {/* <Navbar title = "TextApp" aboutText = "About"/> */}
+        <Navbar
+          title="TextApp"
+          mode={mode}
+          toggleMode={toggleMode}
+          toggleColor={toggleColor}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          {/* <Routes>
+            <Route exact path="/About" element={<About />}></Route>
+            <Route
+              exact path="/"
+              element={ */}
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze"
+                  mode={mode}
+                />
+              {/* }
+            ></Route>
+          </Routes> */}
+        </div>
+      {/* </BrowserRouter> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Uppercase", "success");
+  };
+
+  const handleDownClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lowercase", "success");
+  };
+  const handleOnChange = (event) => {
+    console.log("On change");
+    setText(event.target.value);
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Text Copied", "success");
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces Removed", "success");
+  };
+
+  const wordCount = (text) => {
+    let c = 0;
+    let b = text.trim().split(" ");
+    if (b[b.length - 1] === "") {
+      c = b.length - 1;
+    } else {
+      c = b.length;
+    }
+    return c;
+  };
+
+  const handleCapitalClick = () => {
+    let c = "";
+    let newText = text.toLowerCase();
+    let a = newText.split(" ");
+    for (let i = 0; i < a.length; i++) {
+      a[i] = a[i].charAt(0).toUpperCase() + a[i].substring(1);
+      c = a.join(" ");
+    }
+    setText(c);
+    props.showAlert("Words Capitalized", "success");
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "black",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+            id="myBox"
+            rows="12"
+          ></textarea>
+        </div>
+        <button
+          className="btn cbtn mx-2 my-2"
+          style={{
+            backgroundColor: props.mode === "light" ? "black" : "white",
+            color: props.mode === "light" ? "white" : "black",
+          }}
+          onClick={handleUpClick}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          className="btn cbtn mx-2 my-2"
+          style={{
+            backgroundColor: props.mode === "light" ? "black" : "white",
+            color: props.mode === "light" ? "white" : "black",
+          }}
+          onClick={handleDownClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          className="btn cbtn mx-2 my-2"
+          style={{
+            backgroundColor: props.mode === "light" ? "black" : "white",
+            color: props.mode === "light" ? "white" : "black",
+          }}
+          onClick={handleCapitalClick}
+        >
+          Convert to Capitalize Word
+        </button>
+        <button
+          className="btn cbtn mx-2 my-2"
+          style={{
+            backgroundColor: props.mode === "light" ? "black" : "white",
+            color: props.mode === "light" ? "white" : "black",
+          }}
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+        <button
+          className="btn cbtn mx-2 my-2"
+          style={{
+            backgroundColor: props.mode === "light" ? "black" : "white",
+            color: props.mode === "light" ? "white" : "black",
+          }}
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your text summmary</h1>
+        <p>
+          {wordCount(text)} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
